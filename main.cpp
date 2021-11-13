105f5c195539b99438d8f9d6dfa09050d2d40cf9
Dasha_Khristoforova <khristoforova.de@phystech.edu>
2021-11-13 12:37:59 +0300

    change in new_branch

--- main.cpp
+++ main.cpp
@@ -5,12 +5,13 @@ int h(int) {
     std::cout << "h";
     std::cout << "a";
     std::cout <<"main.cpp change\n";
-
+    std::cout <<"change in new_branch\n";
 }
 
 
 int main() {
     std::cout << "Hello, bear!\n";
     std::cout << "Goodbye, world!\n";
+    std::cout << "See you soon,world!\n";
 }
 
